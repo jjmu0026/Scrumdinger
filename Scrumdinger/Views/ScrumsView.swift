03e06f0a217a3ac94adae7855521871e83bd68b6
Um, Jungmin <jum9@gatech.edu>
2025-05-18 13:55:31 -0400

    App set up

---
+++ Scrumdinger/Views/ScrumsView.swift
@@ -0,0 +1,35 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by mina um on 5/15/25.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    @Binding var scrums: [DailyScrum]
+    
+    var body: some View {
+        NavigationStack {
+            List($scrums) { $scrum in
+                NavigationLink(destination: DetailView(scrum: $scrum)) {
+                    CardView(scrum: scrum)
+                }
+                .listRowBackground(scrum.theme.mainColor)
+            }
+            .navigationTitle("Daily Scrums")
+            .toolbar {
+                Button(action: {}) {
+                    Image(systemName: "plus")
+                }
+                .accessibilityLabel("New Scrum")
+            }
+        }
+    }
+}
+
+#Preview {
+    @Previewable @State var scrums = DailyScrum.sampleData
+    ScrumsView(scrums: $scrums)
+}
